dffe4f8f8a11f4e382fab00010efb8b09517b16b
Vinicius Mendite <vinicius.miff@gmail.com>
2020-07-26 21:47:25 -0300

    fazendo busca na api, e salvando no realmdb

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {StatusBar, Keyboard} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+import getRealm from '../../services/Realm';
 
 import Repository from '../../components/Repository';
 
 import {Container, Title, Form, Input, Submit, List} from './styles';
 
 const Main = () => {
+  const [input, setInput] = useState('');
+  const [error, setError] = useState(false);
+
+  const saveRepository = async (repository) => {
+    const data = {
+      id: repository.id,
+      name: repository.name,
+      fullName: repository.full_name,
+      description: repository.description,
+      stars: repository.stargazers_count,
+      forks: repository.forks_count,
+    };
+
+    const realm = await getRealm(); // recebe a conexão com o bd
+
+    // toda vez que for fazer uma alteração dento do bd, seja adição, edição ou remoção
+    // de algum registro do Realm, é necessário encapsular dentro do write()
+    realm.write(() => {
+      // salvar dados no bd, primeiro passa o nome do schema e depois os dados
+      realm.create('Repository', data);
+    });
+  };
+
+  const handleAddRepository = async () => {
+    try {
+      const response = await api.get(`repos/${input}`);
+
+      saveRepository(response.data);
+
+      setInput('');
+      setError(false);
+      Keyboard.dismiss();
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
     <>
       <StatusBar backgroundColor="transparent" translucent />
@@ -15,18 +55,21 @@ const Main = () => {
 
         <Form>
           <Input
+            value={input}
+            error={error}
+            onChangeText={(t) => setInput(t)}
             autoCapitalize="none"
             autoCorrect={false}
             placeholder="Procurar repositório..."
           />
 
-          <Submit onPress={() => {}}>
+          <Submit onPress={handleAddRepository}>
             <Icon name="add" size={22} color="#fff" />
           </Submit>
         </Form>
 
         <List
-          keyboardShouldPersistTaps="handle"
+          keyboardShouldPersistTaps="handled"
           data={[
             {
               id: 1,
